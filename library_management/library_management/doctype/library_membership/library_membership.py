598f5a0fb6a7b1880136140b367d1a3c9d82ebbf
Ananyobrata Pal <ananyo141@gmail.com>
2024-07-13 01:14:50 +0530

    feat: add full name field in library member, check if membership already exists

--- library_management/library_management/doctype/library_membership/library_membership.py
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2024, ananyo and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.model.docstatus import DocStatus
 from frappe.model.document import Document
 
 
 class LibraryMembership(Document):
-	pass
+    def before_submit(self):
+        exists = frappe.db.exists(
+            "Library Membership",
+            {
+                "library_member": self.library_member,
+                "docstatus": DocStatus.submitted(),
+                "to_date": (">", self.from_date),
+            },
+        )
+        if exists:
+            frappe.throw("There is an active membership for this user")
